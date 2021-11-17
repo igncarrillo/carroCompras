584d25e885ffa89adac92c4971a0d3ac75ecf16a
Joaquin Hernandez <kakihernandez1@gmail.com>
2021-11-17 14:37:53 -0300

    Added ProductoItem - Edited Navbar,Products

--- front/carro-compras/src/components/ProductoItem.js
+++ front/carro-compras/src/components/ProductoItem.js
@@ -1,15 +1,38 @@
-import React from 'react'
+import React from "react";
+import "bootstrap/dist/css/bootstrap.css";
 
 const ProductoItem = ({ data, addToCart }) => {
-    let { id, name, price } = data;
+  let { id, name, price, description } = data;
 
-    return (
-        <div style={{ border: "thin solid gray", padding: "1rem" }}>
-            <h4>{name}</h4>
-            <h5>${price}.00</h5>
-            <button onClick={() => addToCart(id)}>Agregar</button>
+  return (
+      <div
+        className="card border-dark bg-secondary"
+        style={{  height: "15rem",marginBottom: "1rem" }}
+        key={id}
+      >
+        <div className="card-body text-center">
+          {/* <img src={""} /> */}
+          <span className="card-title">{name}</span>
+          <div className="card-text">
+            <p>{description}</p>
+            <p>
+              <b>Precio: ${price}</b>
+            </p>
+          </div>
+          <span
+            to="/"
+            className="btn-floating halfway-fab waves-effect waves-light red"
+            onClick={() => {
+              this.handleClick(id);
+            }}
+          >
+            <a href="#" className="btn btn-primary">
+              Añadir al carro
+            </a>
+          </span>
         </div>
+      </div>
   );
 };
 
-export default ProductoItem
+export default ProductoItem;
--- front/carro-compras/src/components/Products.js
+++ front/carro-compras/src/components/Products.js
@@ -1,48 +1,52 @@
-import React from 'react'
-import { useEffect, useState } from 'react'
+import React from "react";
+import { useEffect, useState } from "react";
+import ProductoItem from "./ProductoItem";
+import "bootstrap/dist/css/bootstrap.css";
 
 const Products = () => {
-    const [contenido, setcontenido] = useState([])
-    const productos = async () =>{
-        const token = window.sessionStorage.getItem("token_product")
-        const response = await fetch('http://localhost:8080/api/products?enabled=true', {
-                method: 'GET',
-                headers: {'Content-Type': 'application/json', 'Authorization': `Bearer ${token}`},
-                credentials: 'include',
-                });
-        const content = await response.json();
-        console.log(content)
-        setcontenido(content)
-    }
-    
-    useEffect(() => {
-        productos()
-    }, [])
-    
-    return (
-        <div>
-            <h2 className="text-center">Productos</h2>
-            <hr/>
-            <div className='row'>
-                <div className='col'>
-                    <h2>Listado de productos</h2>
-                    <ul className='list-group'>
-                        {
-                            contenido.map(item =>
-                                <li key={item.id} className='list-group-item'>
-                                    {item.name}
-                                    {item.description}
-                                    {item.price}
-                                    
-                                </li>
-                                )
-                        }
-                    </ul>
-                </div>
-            </div>
-            
-        </div>
-    )
-}
+  const [contenido, setcontenido] = useState([]);
+  const productos = async () => {
+    const token = window.sessionStorage.getItem("token_product");
+    const response = await fetch(
+      "http://localhost:8080/api/products?enabled=true",
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+        credentials: "include",
+      }
+    );
+    const content = await response.json();
+    console.log(content);
+    setcontenido(content);
+  };
 
-export default Products
+  useEffect(() => {
+    productos();
+  }, []);
+
+  return (
+    <div>
+      <h2 className="text-center">Productos</h2>
+      <hr />
+      {/* <div className="row"> */}
+      {/* <div className="col"> */}
+      <h2>Listado de productos</h2>
+      {/* <ul className="list-group"> */}
+
+          <div className="row" style={{marginInline: "auto"}}>
+      {contenido.map(
+        (item) => (
+          <div className="col ">
+              <ProductoItem data={item} />
+              </div>
+              ))}
+            
+          </div>
+    </div>
+  );
+};
+
+export default Products;
